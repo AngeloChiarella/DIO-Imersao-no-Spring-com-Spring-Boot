e7b309ca6a5720196533c0883148de2e913c5627
AngeloChiarella <chiarella.gaetano@gmail.com>
2022-08-18 11:01:47 -0300

    Beans vs Components

---
+++ src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -0,0 +1,30 @@
+package dio.springboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.google.gson.Gson;
+
+import dio.springboot.app.aula2.ConversorJson;
+import dio.springboot.app.aula2.ViaCepResponse;
+
+@SpringBootApplication
+public class PrimeirosPassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirosPassosApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\":\"01001-000\",\"logradouro\":\"Praça da Sé\", \"cidade\":\"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
+	}
+
+
+}
